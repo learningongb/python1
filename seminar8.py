acb6605d6e6f9331dc5e311063fb43db7412a772
Netkachev <mnetckachev@yandex.ru>
2023-07-12 12:41:39 +0300

    Добавил удаление и изменение записи

--- seminar8.py
+++ seminar8.py
@@ -1,10 +1,13 @@
 from tabulate import tabulate
 
 def bold_green(str):
-    return "\033[1m\033[92m" + str +"\033[0m"; 
+    return "\033[1m\033[92m" + str +"\033[0m" 
 
 def header_color(str):
-    return "\033[95m" + str + "\033[0m"; 
+    return "\033[95m" + str + "\033[0m"
+
+def red(str):
+    return "\033[91m" + str + "\033[0m" 
 
 def show_menu():
     print("\nВыберите необходимое действие:",
@@ -12,8 +15,10 @@ def show_menu():
           bold_green("2") + ". Найти абонента по фамилии",
           bold_green("3") + ". Найти абонента по номеру телефона",
           bold_green("4") + ". Добавить абонента в справочник",
-          bold_green("5") + ". Сохранить справочник в текстовом формате",
-          bold_green("6") + ". Закончить работу",
+          bold_green("5") + ". Удалить абонента по номеру телефона",
+          bold_green("6") + ". Изменить абонента по номеру телефона",
+          bold_green("9") + ". Сохранить справочник в текстовом формате",
+          bold_green("0") + ". Закончить работу",
           sep="\n")
     choice = int(input())
     return choice
@@ -34,6 +39,9 @@ def read_csv(filename: str) -> list:
             data.append(record)
     return data
 
+def write_phonebook(phone_book):
+    write_csv('phonebook.csv', phone_book)
+
 def fields():
     return ["Фамилия", "Имя", "Телефон", "Описание"]    
 
@@ -52,7 +60,7 @@ def print_result(phone_book):
                    tablefmt="rounded_grid"))
     
 def print_serch_result(search_result):
-    print_result(search_result) if search_result else print("\033[91mАбонент не найден\033[0m")
+    print_result(search_result) if search_result else print(red("Абонент не найден"))
 
 def get_search_name():
     return input("Введите фамилию\n").strip()
@@ -73,6 +81,15 @@ def find_by_field(phone_book, field, value):
             result.append(record)
     return result
 
+def find_index_by_number(phone_book, number):
+    return find_index_by_field(phone_book, "Телефон", number)
+
+def find_index_by_field(phone_book, field, value):
+    for index in range(len(phone_book)):
+        if phone_book[index][field].lower() == value.lower():
+            return index
+    return -1
+
 def get_new_user():
     user = dict()
     for field in fields():
@@ -82,11 +99,28 @@ def get_new_user():
 def add_user(phone_book: list, user_data):
     phone_book.append(user_data)
 
+def del_user(phone_book: list, index: int):
+    if index < 0:
+        print(red("Абонент не найден"))
+        return False, phone_book
+    return True, phone_book[:index] + phone_book[index + 1:]
+
+def change_user(phone_book):
+    index = find_index_by_number(phone_book, get_search_number())
+    if index < 0:
+        print(red("Абонент не найден"))
+        return False, phone_book
+    print("Введите новые данные абонента")
+    return True, change_record(phone_book, index, get_new_user())
+
+def change_record(phone_book, index, record):
+    return phone_book[:index] + [record] +  phone_book[index + 1:]
+
 def work_with_phonebook():
     choice = show_menu()
     phone_book = read_csv('phonebook.csv')
 
-    while (choice != 6):
+    while (choice != 0):
         if choice == 1:
             print_result(phone_book)
         elif choice == 2:
@@ -98,8 +132,17 @@ def work_with_phonebook():
         elif choice == 4:
             user_data = get_new_user()
             add_user(phone_book, user_data)
-            write_csv('phonebook.csv', phone_book)
+            write_phonebook(phone_book)
         elif choice == 5:
+            number = get_search_number()
+            modified, phone_book = del_user(phone_book, find_index_by_number(phone_book, number))
+            if modified:
+                write_phonebook(phone_book)
+        elif choice == 6:
+            modified, phone_book = change_user(phone_book)
+            if modified:
+                write_phonebook(phone_book)
+        elif choice == 9:
             file_name = get_file_name()
             write_txt(file_name, phone_book)
         choice = show_menu()
